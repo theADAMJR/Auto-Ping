740d77e07e7ae5ae5ec19809331fd5e7bd78b737
theADAMJR <adamjf123@gmail.com>
2020-07-09 15:37:11 +0100

    add multiple url support

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const fetch = require('node-fetch');
 
 const config = {
   interval: 5 * 60 * 1000,
-  url: 'https://theshrekbot.glitch.me/'
+  urls: ['https://1pg.glitch.me/', 'https://theshrekbot.glitch.me']
 }
 
 console.log(`Next ping in ${config.interval / 60 / 1000} min(s)`);
 
-let i = 0;
+let pings = 0;
 setInterval(() => {
-  fetch(config.url).then(() => console.log(`[${++i}] Pinged ${config.url}`));
+  pings++;
+  for (const url of config.urls)
+    fetch(config.url).then(() => console.log(`[${pings}] Pinged ${config.url}`));
 }, config.interval);
